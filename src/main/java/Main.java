781a80bfbce86e8e2dd306f19abbe32306372953
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-04-26 08:46:58 +0100

    refactored uml diagram

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,45 @@ public class Main {
 
 /*
 
-        * ----------------------- * -------------------------- * ---------------------------- *
-        |    Football Teams  (animal)     |    League    (enclosure)              |         Country     zoo         |  l
-        * --------------------- * ---------------------------- * ---------------------------- *
-        |   - Name: String        |   - Name: String           |   - Name: String             |
-        |   - Location: String    |  - Teams ArrayList<Teams>  |
-        |   - League: String      |  - Referees: String        |                              |
-        |    - Position: int      |  -        |                              |
-        |     -doTheyTrain()                 |           boolean          |                              |
-        * --------------------- * ---------------------------- * ---------------------------- *
-        |  + Getters/Setters      | + Getters/Setters           | + countStudents(): int       |
-        |                         |                             |
-        |   + isTraining         |    +addTeam(Teams)          |
+        * -------------------------- * -------------------------- * ---------------------------- *
+        |    Football Teams  (animal) |    League (enclosure)     |         Country     zoo      |
+        * -------------------------- * ---------------------------- * ---------------------------*
+        |   - Name: String           |   - Name: String           |   - Name: String             |
+        |   - Location: String       |  - Teams ArrayList<Teams>  |                              |
+        |   - League: String         |  -                         |                              |
+        |    - Position: int         |  -                         |                              |
+        |     -doTheyTrain()         |                            |                              |
+        * -------------------------- * --------------------------- * --------------------------- *
+        |  + Getters/Setters         | + Getters/Setters           | + countStudents(): int       |
+        |                            |                             |                              |
+        |   + isTraining             |    +addTeam(Teams)          |                              |
         |    +isNotTraining          |                             |  + addNewStudent(Student):   |
-        |    + Shoutchant()       |  + countTeams(): int        |      void                    |
-        |            String       |    + playing                |                              |
-        |                         |                             |                              |
-        |                         |   + n      |                              |
-        * --------------------- * --------------------- * ---------------------------- *
+        |    + Shoutchant()          |  + countTeams(): int        |      void                    |
+        |            String          |                             |                              |
+        |                            |                             |                              |
+        |                            |                             |                              |
+        * --------------------- -----* --------------------------- * ---------------------------- *
+
+
+         * -------------------------- * -------------------------- *
+        |   TopFive (Birdhouse)       |    London Teams (Birds)     |
+        * -------------------------- * ---------------------------- *
+        |   - Name: String           |   - Name: String             |
+        |   - Location: String       |  - Location: String          |   
+        |   - League: String         |  -  League: String           |
+        |    - Position: int         |  -                           |
+        |     -doTheyTrain()         |                              |
+        * -------------------------- * --------------------------- *
+        |  + Getters/Setters         | + Getters/Setters           |
+        |                            |                             |
+        |   + isTraining             |    +addTeam(Teams)          |
+        |    +isNotTraining          |                             |
+        |    + Shoutchant()          |  + countTeams(): int        |
+        |            String          |                             |
+        |                            |                             |
+        |                            |                             |
+        * --------------------- -----* --------------------------- *
+
 
 Zoo - Country
 Enclosure - League
@@ -27,7 +48,7 @@ Animals - Football Teams
 Birdhouse - TopFive
 Birds - London Teams
 Penguin/Parrot - Arsenal/Chelsea
-Lion
+Staff/Keeper - Ref?Players?
 
  */
 
